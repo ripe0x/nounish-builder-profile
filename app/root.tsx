71a39e6aabf80b0a90e59039ac581ee5972e56fb
ripe <unripeengine@gmail.com>
2022-10-19 18:20:33 -0700

    add og and twitter meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -21,8 +21,18 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "ripe / nounish builder profile ⌐◨-◨" ,
+  description: "dad. designer. developer. nounish product builder.",
   viewport: "width=device-width,initial-scale=1",
   "og:image": "https://www.ripe.wtf/share.png", 
+  "og:type": "website",
+  "og:url": "https://www.ripe.wtf/",
+  "og:title": "ripe / nounish builder profile ⌐◨-◨",
+  "og:description": "dad. designer. developer. nounish product builder.",
+  "twitter:card": "summary_large_image",
+  "twitter:url": "https://www.ripe.wtf/",
+  "twitter:title": "ripe / nounish builder profile ⌐◨-◨",
+  "twitter:description": "dad. designer. developer. nounish product builder.",
+  "twitter:image": "https://www.ripe.wtf/share.png", 
 });
 
 export default function App() {
